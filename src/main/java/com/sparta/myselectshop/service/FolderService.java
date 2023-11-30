7abe20ff3c88a326b23dc418c9e9a41c27095f71
yongcowhee <yongcowhee@gmail.com>
2023-11-30 20:31:13 +0900

    feat: 상품 페이징 및 정렬

---
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -0,0 +1,61 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.FolderResponseDto;
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.FolderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService {
+
+    private final FolderRepository folderRepository;
+
+    // 로그인한 회원에 폴더들 등록
+    public void addFolders(List<String> folderNames, User user) {
+
+        // 입력으로 들어온 폴더 이름을 기준으로, 회원이 이미 생성한 폴더들을 조회합니다.
+        List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
+
+        List<Folder> folderList = new ArrayList<>();
+
+        for (String folderName : folderNames) {
+            // 이미 생성한 폴더가 아닌 경우만 폴더 생성
+            if (!isExistFolderName(folderName, existFolderList)) {
+                Folder folder = new Folder(folderName, user);
+                folderList.add(folder);
+            } else {
+                throw new IllegalArgumentException("폴더명이 중복되었습니다.");
+            }
+        }
+
+        folderRepository.saveAll(folderList);
+    }
+
+    private Boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
+        // 기존 폴더 리스트에서 folder name 이 있는지?
+        for (Folder existFolder : existFolderList) {
+            if(folderName.equals(existFolder.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 로그인한 회원이 등록된 모든 폴더 조회
+    public List<FolderResponseDto> getFolders(User user) {
+        List<Folder> folderList = folderRepository.findAllByUser(user);
+        List<FolderResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Folder folder : folderList) {
+            responseDtoList.add(new FolderResponseDto(folder));
+        }
+
+        return responseDtoList;
+    }
+}
