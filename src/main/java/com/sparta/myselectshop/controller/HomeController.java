59830a165183c65fdd3579b7bf159e83c0811c9c
yongcowhee <yongcowhee@gmail.com>
2023-11-30 22:39:08 +0900

    feat: 폴더 별 관심 상품 조회

--- src/main/java/com/sparta/myselectshop/controller/HomeController.java
+++ src/main/java/com/sparta/myselectshop/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.sparta.myselectshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
+    Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
